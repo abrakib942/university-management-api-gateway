59245fdc33c3e628b5192b5b62c1b0f83e0ca814
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-09-16 19:39:28 +0600

    connect with redis

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/v1', routes);
+// app.use('/api/v1', routes);
+
+app.get('/', (req, res) => {
+  res.send('Server started successfully');
+});
 
 app.use(globalExceptionHandler);
 
